000cea945574f11b54513dbc561e952ac797fa15
Nate Todd <nate@natetodd.com>
2009-07-19 00:22:18 -0400

    changed some text and turned the logger off

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ RACK_ENV = ENV['RACK_ENV'] || 'development' unless defined? RACK_ENV
 ActiveRecord::Base.establish_connection YAML.load(File.read(File.join(File.dirname(__FILE__), 'config', 'database.yml'))).with_indifferent_access[RACK_ENV]
 
 # Setup logger for activerecord
-ActiveRecord::Base.logger = Logger.new(File.open(File.join(File.dirname(__FILE__), 'log', "#{RACK_ENV}.log"), 'a'))
+# ActiveRecord::Base.logger = Logger.new(File.open(File.join(File.dirname(__FILE__), 'log', "#{RACK_ENV}.log"), 'a'))
 
 # Load libs from lib/
 Dir[File.join(File.dirname(__FILE__), 'lib', '*.rb')].each {|lib| require lib }
