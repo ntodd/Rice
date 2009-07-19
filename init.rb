be587212036968d10a903a129a422f48e818bfdf
Nate Todd <nate@natetodd.com>
2009-07-19 00:23:37 -0400

    another db test for heroku

--- init.rb
+++ init.rb
@@ -2,10 +2,13 @@ require 'activesupport'
 require 'activerecord'
 
 # Find environment
-RACK_ENV = ENV['RACK_ENV'] || 'development' unless defined? RACK_ENV
+# RACK_ENV = ENV['RACK_ENV'] || 'development' unless defined? RACK_ENV
+
+dbconfig = YAML.load(File.read('config/database.yml'))
+ActiveRecord::Base.establish_connection dbconfig['production']
 
 # Load db config and establish connection
-ActiveRecord::Base.establish_connection YAML.load(File.read(File.join(File.dirname(__FILE__), 'config', 'database.yml'))).with_indifferent_access[RACK_ENV]
+# ActiveRecord::Base.establish_connection YAML.load(File.read(File.join(File.dirname(__FILE__), 'config', 'database.yml'))).with_indifferent_access[RACK_ENV]
 
 # Setup logger for activerecord
 # ActiveRecord::Base.logger = Logger.new(File.open(File.join(File.dirname(__FILE__), 'log', "#{RACK_ENV}.log"), 'a'))
