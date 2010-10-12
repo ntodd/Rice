5c61feb1b6b677c8cc1683d04d86aa6030133861
Nate Todd <nate@natetodd.com>
2010-10-11 21:17:27 -0400

    updated sha1 algorithm with a faster one

--- public/js/sha1.js
+++ public/js/sha1.js
@@ -1,167 +1,172 @@
-function SHA1 (msg) {
+var hexcase = 0;  /* hex output format. 0 - lowercase; 1 - uppercase        */
+var b64pad  = ""; /* base-64 pad character. "=" for strict RFC compliance   */
+var chrsz   = 8;  /* bits per input character. 8 - ASCII; 16 - Unicode      */
  
-	function rotate_left(n,s) {
-		var t4 = ( n<<s ) | (n>>>(32-s));
-		return t4;
-	};
+function SHA1(s){return binb2hex(core_sha1(str2binb(s),s.length * chrsz));}
+  
+/*
+ * Calculate the SHA-1 of an array of big-endian words, and a bit length
+ */
+function core_sha1(x, len)
+{
+  /* append padding */
+  x[len >> 5] |= 0x80 << (24 - len % 32);
+  x[((len + 64 >> 9) << 4) + 15] = len;
  
-	function lsb_hex(val) {
-		var str="";
-		var i;
-		var vh;
-		var vl;
+  var w = Array(80);
+  var a =  1732584193;
+  var b = -271733879;
+  var c = -1732584194;
+  var d =  271733878;
+  var e = -1009589776;
  
-		for( i=0; i<=6; i+=2 ) {
-			vh = (val>>>(i*4+4))&0x0f;
-			vl = (val>>>(i*4))&0x0f;
-			str += vh.toString(16) + vl.toString(16);
-		}
-		return str;
-	};
+  for(var i = 0; i < x.length; i += 16)
+  {
+    var olda = a;
+    var oldb = b;
+    var oldc = c;
+    var oldd = d;
+    var olde = e;
  
-	function cvt_hex(val) {
-		var str="";
-		var i;
-		var v;
+    for(var j = 0; j < 80; j++)
+    {
+      if(j < 16) w[j] = x[i + j];
+      else w[j] = rol(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1);
+      var t = safe_add(safe_add(rol(a, 5), sha1_ft(j, b, c, d)),
+                       safe_add(safe_add(e, w[j]), sha1_kt(j)));
+      e = d;
+      d = c;
+      c = rol(b, 30);
+      b = a;
+      a = t;
+    }
  
-		for( i=7; i>=0; i-- ) {
-			v = (val>>>(i*4))&0x0f;
-			str += v.toString(16);
-		}
-		return str;
-	};
- 
- 
-	function Utf8Encode(string) {
-		string = string.replace(/\r\n/g,"\n");
-		var utftext = "";
- 
-		for (var n = 0; n < string.length; n++) {
- 
-			var c = string.charCodeAt(n);
- 
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
- 
-		}
- 
-		return utftext;
-	};
- 
-	var blockstart;
-	var i, j;
-	var W = new Array(80);
-	var H0 = 0x67452301;
-	var H1 = 0xEFCDAB89;
-	var H2 = 0x98BADCFE;
-	var H3 = 0x10325476;
-	var H4 = 0xC3D2E1F0;
-	var A, B, C, D, E;
-	var temp;
- 
-	msg = Utf8Encode(msg);
- 
-	var msg_len = msg.length;
- 
-	var word_array = new Array();
-	for( i=0; i<msg_len-3; i+=4 ) {
-		j = msg.charCodeAt(i)<<24 | msg.charCodeAt(i+1)<<16 |
-		msg.charCodeAt(i+2)<<8 | msg.charCodeAt(i+3);
-		word_array.push( j );
-	}
- 
-	switch( msg_len % 4 ) {
-		case 0:
-			i = 0x080000000;
-		break;
-		case 1:
-			i = msg.charCodeAt(msg_len-1)<<24 | 0x0800000;
-		break;
- 
-		case 2:
-			i = msg.charCodeAt(msg_len-2)<<24 | msg.charCodeAt(msg_len-1)<<16 | 0x08000;
-		break;
- 
-		case 3:
-			i = msg.charCodeAt(msg_len-3)<<24 | msg.charCodeAt(msg_len-2)<<16 | msg.charCodeAt(msg_len-1)<<8	| 0x80;
-		break;
-	}
- 
-	word_array.push( i );
- 
-	while( (word_array.length % 16) != 14 ) word_array.push( 0 );
- 
-	word_array.push( msg_len>>>29 );
-	word_array.push( (msg_len<<3)&0x0ffffffff );
- 
- 
-	for ( blockstart=0; blockstart<word_array.length; blockstart+=16 ) {
- 
-		for( i=0; i<16; i++ ) W[i] = word_array[blockstart+i];
-		for( i=16; i<=79; i++ ) W[i] = rotate_left(W[i-3] ^ W[i-8] ^ W[i-14] ^ W[i-16], 1);
- 
-		A = H0;
-		B = H1;
-		C = H2;
-		D = H3;
-		E = H4;
- 
-		for( i= 0; i<=19; i++ ) {
-			temp = (rotate_left(A,5) + ((B&C) | (~B&D)) + E + W[i] + 0x5A827999) & 0x0ffffffff;
-			E = D;
-			D = C;
-			C = rotate_left(B,30);
-			B = A;
-			A = temp;
-		}
- 
-		for( i=20; i<=39; i++ ) {
-			temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0x6ED9EBA1) & 0x0ffffffff;
-			E = D;
-			D = C;
-			C = rotate_left(B,30);
-			B = A;
-			A = temp;
-		}
- 
-		for( i=40; i<=59; i++ ) {
-			temp = (rotate_left(A,5) + ((B&C) | (B&D) | (C&D)) + E + W[i] + 0x8F1BBCDC) & 0x0ffffffff;
-			E = D;
-			D = C;
-			C = rotate_left(B,30);
-			B = A;
-			A = temp;
-		}
- 
-		for( i=60; i<=79; i++ ) {
-			temp = (rotate_left(A,5) + (B ^ C ^ D) + E + W[i] + 0xCA62C1D6) & 0x0ffffffff;
-			E = D;
-			D = C;
-			C = rotate_left(B,30);
-			B = A;
-			A = temp;
-		}
- 
-		H0 = (H0 + A) & 0x0ffffffff;
-		H1 = (H1 + B) & 0x0ffffffff;
-		H2 = (H2 + C) & 0x0ffffffff;
-		H3 = (H3 + D) & 0x0ffffffff;
-		H4 = (H4 + E) & 0x0ffffffff;
- 
-	}
- 
-	var temp = cvt_hex(H0) + cvt_hex(H1) + cvt_hex(H2) + cvt_hex(H3) + cvt_hex(H4);
- 
-	return temp.toLowerCase();
+    a = safe_add(a, olda);
+    b = safe_add(b, oldb);
+    c = safe_add(c, oldc);
+    d = safe_add(d, oldd);
+    e = safe_add(e, olde);
+  }
+  return Array(a, b, c, d, e);
  
 }
+ 
+/*
+ * Perform the appropriate triplet combination function for the current
+ * iteration
+ */
+function sha1_ft(t, b, c, d)
+{
+  if(t < 20) return (b & c) | ((~b) & d);
+  if(t < 40) return b ^ c ^ d;
+  if(t < 60) return (b & c) | (b & d) | (c & d);
+  return b ^ c ^ d;
+}
+ 
+/*
+ * Determine the appropriate additive constant for the current iteration
+ */
+function sha1_kt(t)
+{
+  return (t < 20) ?  1518500249 : (t < 40) ?  1859775393 :
+         (t < 60) ? -1894007588 : -899497514;
+}
+ 
+/*
+ * Calculate the HMAC-SHA1 of a key and some data
+ */
+// function core_hmac_sha1(key, data)
+// {
+//   var bkey = str2binb(key);
+//   if(bkey.length > 16) bkey = core_sha1(bkey, key.length * chrsz);
+//  
+//   var ipad = Array(16), opad = Array(16);
+//   for(var i = 0; i < 16; i++)
+//   {
+//     ipad[i] = bkey[i] ^ 0x36363636;
+//     opad[i] = bkey[i] ^ 0x5C5C5C5C;
+//   }
+//  
+//   var hash = core_sha1(ipad.concat(str2binb(data)), 512 + data.length * chrsz);
+//   return core_sha1(opad.concat(hash), 512 + 160);
+// }
+ 
+/*
+ * Add integers, wrapping at 2^32. This uses 16-bit operations internally
+ * to work around bugs in some JS interpreters.
+ */
+function safe_add(x, y)
+{
+  var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+  var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+  return (msw << 16) | (lsw & 0xFFFF);
+}
+ 
+/*
+ * Bitwise rotate a 32-bit number to the left.
+ */
+function rol(num, cnt)
+{
+  return (num << cnt) | (num >>> (32 - cnt));
+}
+ 
+/*
+ * Convert an 8-bit or 16-bit string to an array of big-endian words
+ * In 8-bit function, characters >255 have their hi-byte silently ignored.
+ */
+function str2binb(str)
+{
+  var bin = Array();
+  var mask = (1 << chrsz) - 1;
+  for(var i = 0; i < str.length * chrsz; i += chrsz)
+    bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (32 - chrsz - i%32);
+  return bin;
+}
+ 
+/*
+ * Convert an array of big-endian words to a string
+ */
+// function binb2str(bin)
+// {
+//   var str = "";
+//   var mask = (1 << chrsz) - 1;
+//   for(var i = 0; i < bin.length * 32; i += chrsz)
+//     str += String.fromCharCode((bin[i>>5] >>> (32 - chrsz - i%32)) & mask);
+//   return str;
+// }
+ 
+/*
+ * Convert an array of big-endian words to a hex string.
+ */
+function binb2hex(binarray)
+{
+  var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+  var str = "";
+  for(var i = 0; i < binarray.length * 4; i++)
+  {
+    str += hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8+4)) & 0xF) +
+           hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8  )) & 0xF);
+  }
+  return str;
+}
+ 
+/*
+ * Convert an array of big-endian words to a base-64 string
+ */
+// function binb2b64(binarray)
+// {
+//   var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+//   var str = "";
+//   for(var i = 0; i < binarray.length * 4; i += 3)
+//   {
+//     var triplet = (((binarray[i   >> 2] >> 8 * (3 -  i   %4)) & 0xFF) << 16)
+//                 | (((binarray[i+1 >> 2] >> 8 * (3 - (i+1)%4)) & 0xFF) << 8 )
+//                 |  ((binarray[i+2 >> 2] >> 8 * (3 - (i+2)%4)) & 0xFF);
+//     for(var j = 0; j < 4; j++)
+//     {
+//       if(i * 8 + j * 6 > binarray.length * 32) str += b64pad;
+//       else str += tab.charAt((triplet >> 6*(3-j)) & 0x3F);
+//     }
+//   }
+//   return str;
+// }
